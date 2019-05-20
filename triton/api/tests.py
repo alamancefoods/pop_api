baf7ce7def666ab24c4d0ece130714cb03d653de
MTempleman <huntertempleman@gmail.com>
2019-05-20 15:48:12 -0600

    Created Models and Dummy Data for Database.
    As the title suggests, the API is taking shape. The models describe basic
    production components, with tables for items, employees, production lines,
    production machines, and shifts. I created a ProdGoal and ProdSnap table that
    describe daily production goals and granular production instances respectively.
    The ProdSnap table will likely grow to be very large if things go as planned.
    
    The csv files in the dummy_data folder get read by the fixture_maker script and
    spit out a json fixture describing dummy data that will seed the database during
    devleopment.
    
    I'm just gonna stick to sqlite for development and jump to postgres once things
    seem ready for production. One of those obvious choices...

--- triton/api/tests.py
+++ triton/api/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from model_mommy import mommy
-from api.models import Item
+from api.models import Item, Employee, Shift, Line, Machine, ProdGoal, ProdSnap
 # Create your tests here.
 
 class ItemTest(TestCase):
@@ -8,3 +8,45 @@ class ItemTest(TestCase):
     def test_item(self):
         item = mommy.make(Item)
         self.assertTrue(isinstance(item, Item))
+
+
+class EmployeeTest(TestCase):
+
+    def test_employee(self):
+        employee = mommy.make(Employee)
+        self.assertTrue(isinstance(employee, Employee))
+
+
+class ShiftTest(TestCase):
+
+    def test_shift(self):
+        shift = mommy.make(Shift)
+        self.assertTrue(isinstance(shift, Shift))
+
+
+class LineTest(TestCase):
+
+    def test_line(self):
+        line = mommy.make(Line)
+        self.assertTrue(isinstance(line, Line))
+
+
+class MachineTest(TestCase):
+
+    def test_machine(self):
+        machine = mommy.make(Machine)
+        self.assertTrue(isinstance(machine, Machine))
+
+
+class ProdGoalTest(TestCase):
+
+    def test_prod_goal(self):
+        prod_goal = mommy.make(ProdGoal)
+        self.assertTrue(isinstance(prod_goal, ProdGoal))
+
+
+class ProdSnapTest(TestCase):
+
+    def test_prod_snap(self):
+        prod_snap = mommy.make(ProdSnap)
+        self.assertTrue(isinstance(prod_snap, ProdSnap))
